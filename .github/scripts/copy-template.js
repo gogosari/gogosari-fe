da76db43c46da34ff0d642da67edc01ac358960e
deun <devdeun@gmail.com>
2025-02-19 16:57:28 +0900

    settings: .github 파일 추가

---
+++ .github/scripts/copy-template.js
@@ -0,0 +1,21 @@
+import { readFileSync, writeFileSync } from 'fs'
+import { join } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const templatePath = join(__dirname, '..', '.gitmessage.txt')
+const targetPath = process.argv[2]
+
+const existingMessage = readFileSync(targetPath, 'utf-8')
+const template = readFileSync(templatePath, 'utf-8')
+
+const hasExistingMessage = existingMessage
+  .split('\n')
+  .some((line) => line.trim() && !line.startsWith('#'))
+
+if (!hasExistingMessage) {
+  writeFileSync(targetPath, template)
+} else {
+  const typeGuide = template.split('################').pop()
+  writeFileSync(targetPath, `${existingMessage}\n${typeGuide}`)
+}
