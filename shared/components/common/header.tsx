a48b2487402b95e67480c5192fcbed4c8fe9a9f9
deun <devdeun@gmail.com>
2025-02-21 00:41:33 +0900

    style: header 스타일 수정

--- shared/components/common/header.tsx
+++ shared/components/common/header.tsx
@@ -6,7 +6,7 @@ import SearchIcon from '@/public/svgs/search.svg'
 export default function Header({ children }: PropsWithChildren) {
   return (
     <>
-      <header className="fixed top-0 z-50 flex h-14 w-full max-w-screen-sm -translate-x-5 items-center justify-end overflow-x-hidden border-b border-gray-200 bg-white px-[22.5px] py-2 sm:w-[390px]">
+      <header className="fixed top-0 z-50 flex h-14 w-full max-w-screen-sm items-center justify-end overflow-x-hidden border-b border-gray-200 bg-white px-[22.5px] py-2 sm:w-[390px]">
         <h1 className="absolute top-1/2 left-1/2 -translate-1/2 font-bold">{children}</h1>
         <div className="flex gap-2">
           <button className="flex items-center justify-center">
