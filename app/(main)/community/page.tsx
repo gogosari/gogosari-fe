3851b02804d64f4060f490b0b4143b2a585228ee
deun <devdeun@gmail.com>
2025-02-20 22:59:29 +0900

    feat: 커뮤니티 페이지 구현

--- app/(main)/community/page.tsx
+++ app/(main)/community/page.tsx
@@ -1,3 +1,15 @@
+import CommunityContentsSection from '@/features/community/components/contents-section'
+import CommunityHotSpotSection from '@/features/community/components/hot-spot-section'
+import CommunityTipSection from '@/features/community/components/tip-section'
+import CommunityVideoGuideSection from '@/features/community/components/video-guide-section'
+
 export default function CommunityPage() {
-  return <div className="flex h-full w-full items-center justify-center">Community Page</div>
+  return (
+    <div className="px-5 pb-[80px]">
+      <CommunityHotSpotSection />
+      <CommunityContentsSection />
+      <CommunityVideoGuideSection />
+      <CommunityTipSection />
+    </div>
+  )
 }
