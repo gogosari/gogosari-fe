1a8caaf081e506d2ded7ef379d3edcd8e025a71d
deun <devdeun@gmail.com>
2025-02-21 00:22:21 +0900

    feat: 커뮤니티 페이지 header 추가

--- app/(main)/community/page.tsx
+++ app/(main)/community/page.tsx
@@ -2,10 +2,12 @@ import CommunityContentsSection from '@/features/community/components/contents-s
 import CommunityHotSpotSection from '@/features/community/components/hot-spot-section'
 import CommunityTipSection from '@/features/community/components/tip-section'
 import CommunityVideoGuideSection from '@/features/community/components/video-guide-section'
+import Header from '@/shared/components/common/header'
 
 export default function CommunityPage() {
   return (
     <div className="px-5 pb-[80px]">
+      <Header>커뮤니티</Header>
       <CommunityHotSpotSection />
       <CommunityContentsSection />
       <CommunityVideoGuideSection />
