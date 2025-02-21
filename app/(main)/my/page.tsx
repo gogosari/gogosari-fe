14c0a8dd2c2b2d81b99337dd169f533aaaca37f5
chan9yu <dev.cgyeo@gmail.com>
2025-02-21 12:56:22 +0900

    feat: 마이페이지 구성

--- app/(main)/my/page.tsx
+++ app/(main)/my/page.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image'
+
+import myImage from '@/public/images/my.png'
+import Header from '@/shared/components/common/header'
+
 export default function MyPage() {
-  return <div className="flex h-full w-full items-center justify-center">My Page</div>
+  return (
+    <div className="flex h-full w-full flex-col items-center">
+      <Header>위치제보</Header>
+      <div className="h-[323px] w-[323px]">
+        <Image src={myImage} alt="my-page" width={323} height={323} />
+      </div>
+    </div>
+  )
 }
