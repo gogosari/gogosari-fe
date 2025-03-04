a51cb7ff6672dd3c33d20167fb4108a2beec2307
deun <devdeun@gmail.com>
2025-03-04 10:21:14 +0900

    fix: safari 폰트 굵기 문제 해결

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ const pretendard = localFont({
   src: '../public/fonts/PretendardVariable.woff2',
   variable: '--font-pretendard',
   display: 'swap',
+  weight: '45 920',
   preload: true,
 })
 
